4b7bb5e1bb3cd97689009d920b4f08d4f69644c2
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-01 19:11:13 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,11 @@
 
 def levitation_quiz
 	#your code here
+  until answer == "Wingardium levitation"
+    puts "What is the spell that enacts levitation?"
+    answer = gets.chomp
+  end
+  puts "You passed the quiz!"
 end
 
 
